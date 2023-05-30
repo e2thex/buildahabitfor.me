e4754819e7eb981466ffe883339d78a939cd9a73
Erik Summerfield <e2thex@eriksummerfield.lan>
2023-05-30 11:08:36 -0500

    added copy to clipboard

--- src/components/HabitTable.tsx
+++ src/components/HabitTable.tsx
@@ -1,12 +1,24 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { HabitData } from './types';
 import HabitContext from './HabitContext';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import UndoIcon from '@mui/icons-material/Undo';
+import copyToClipboard from 'copy-to-clipboard';
+import CopyIcon from '@mui/icons-material/ContentCopy';
+import CheckIcon from '@mui/icons-material/Check';
 import { v4 } from 'uuid';
 const HabitTable = () => { 
     const {data, setCurrent,remove, currentId} = useContext(HabitContext);
+    const [copied, setCopied] = useState(false);
+    const copy = () => {
+      const text = data.map((item) => {
+        return `When ${item[0]}, instead of ${item[1]}, I will ${item[2]}.`;
+      }).join('\n');
+      setCopied(true);
+      setTimeout(() => { setCopied(false); }, 2000);
+      copyToClipboard(text);
+    }
     return (
       <table className='w-full'>
         <thead>
@@ -14,7 +26,12 @@ const HabitTable = () => {
             <th className='w-[30%] text-lg p-2 my-2'>When</th>
             <th className='w-[30%] text-lg p-2 my-2'>Instead of</th>
             <th className='w-[30%] text-lg p-2 my-2'>I will</th>
-            <th className='w-[10%] text-lg p-2 my-2'></th>
+            <th className='w-[10%] text-lg p-2 my-2 relative'>
+              <div className='relative'>
+              <CopyIcon className='hover:text-blue-300 cursor-pointer' onClick={copy}></CopyIcon>
+              {copied ? <CheckIcon className="absolute text-green-500"></CheckIcon> : <></>}
+              </div>
+              </th>
           </tr>
         </thead>
         <tbody>
@@ -27,11 +44,11 @@ const HabitTable = () => {
                 <td className='text-lg p-2 my-2 align-top'>
                   {item[3]!==currentId ?
                     <>
-                      <EditIcon className='cursor-pointer' onClick={(e) => setCurrent(item[3])} /> 
-                      <DeleteIcon className='cursor-pointer' onClick={(e) => remove(item[3])} /> 
+                      <EditIcon className='hover:text-blue-300 cursor-pointer' onClick={(e) => setCurrent(item[3])} /> 
+                      <DeleteIcon className='hover:text-blue-300 cursor-pointer' onClick={(e) => remove(item[3])} /> 
                     </>
                     : 
-                      <UndoIcon className='cursor-pointer' onClick={(e) => setCurrent(v4())} /> 
+                      <UndoIcon className='hover:text-blue-300 cursor-pointer' onClick={(e) => setCurrent(v4())} /> 
                    }
                   </td>
               </tr>
@@ -42,3 +59,4 @@ const HabitTable = () => {
     )
   }
 export default HabitTable;
+
