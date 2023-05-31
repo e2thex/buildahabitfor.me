ef070a2d392ce2047c2ccf705d3b6dd454d1e1d3
Erik Summerfield <e2thex@eriksummerfield.lan>
2023-05-31 08:39:27 -0500

    updated to filter on the cohort

--- convex/getHabits.ts
+++ convex/getHabits.ts
@@ -1,5 +1,5 @@
 import { query } from "./_generated/server";
 
-export default query(async ({ db }) => {
-  return await db.query("habits").collect();
+export default query(async ({ db }, { cohort }) => {
+  return await db.query("habits").filter(q => q.eq(q.field("cohort"), cohort)).collect();
 });
