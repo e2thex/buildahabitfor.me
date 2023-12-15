9ffa656d9fcaf29fb75e810d51416710f5add191
Erik Summerfield <e2thex@eriksummerfield.lan>
2023-12-15 11:02:51 -0600

    design updates

--- src/components/HabitReward.tsx
+++ src/components/HabitReward.tsx
@@ -47,8 +47,8 @@ const HabitReward = (props:Props) => {
     return (
       <>
       <div className='bg-black w-full font-raleway bg-contain bg-right-bottom bg-no-repeat' style={{backgroundImage: "url(/dark_grey_path-06.png)"}}>
-        <h1 className='text-6xl text-white py-5'>Congratulations {author}!</h1>
-        <h2 className='text-3xl text-white'>On completing Phase2’s Coaching Great Books Course.</h2>
+        <h1 className='text-6xl font-bold text-white py-5'>Congratulations {author}!</h1>
+        <h2 className='text-3xl font-bold text-white'>On completing Phase2’s Coaching Great Books Course.</h2>
         <img src='./quotes-06.png' className='w-2/12 my-5'/>
         <HabitList {...{author}} />
         <img src='./quotes-06-end.png' className='w-2/12 my-5 ml-auto' />
